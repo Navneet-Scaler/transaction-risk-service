ad0166799bc5e1f32b26eafef914b2a4b93f1d41
Navneet-Scaler <navneet.23bcs10136@ms.sst.scaler.com>
2026-01-20 17:57:48 +0530

    Restructure repo: move Spring Boot project to root

---
+++ src/main/java/com/example/transaction_risk_service/TransactionRiskServiceApplication.java
@@ -0,0 +1,13 @@
+package com.example.transaction_risk_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TransactionRiskServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TransactionRiskServiceApplication.class, args);
+	}
+
+}
